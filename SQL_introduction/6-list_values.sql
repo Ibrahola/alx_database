6b87029e23acb7bfb3f90425df63a01548959545
Ibraheem Bakare <bakare.ibraheem.ola@gmail.com>
2023-08-19 18:01:51 +0100

    sql tutorial

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,6 +1,5 @@
 -- This selects rows in table
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
-SHOW DATABASES;
 USE hbtn_0c_0;
 CREATE TABLE IF NOT EXISTS  first_table (id int, name varchar(256));
 SELECT *FROM first_table;
