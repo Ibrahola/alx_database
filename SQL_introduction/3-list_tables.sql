c1af5a410b603246e0fcccd8ec686cc2e9de2f64
Ibraheem Bakare <bakare.ibraheem.ola@gmail.com>
2023-08-19 17:33:57 +0100

    sql tutorial

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,5 +1,4 @@
 -- This lists tables in a database.
-CREATE DATABASE IF NOT EXISTS hbtn_db_0;
-SHOW DATABASES;
-USE hbtn_db_0;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_0;
+USE hbtn_test_db_0;
 SHOW Tables;
