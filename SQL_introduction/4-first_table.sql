8ef4eee5a9c50aff0bdbf8dbe3f002c3218491c0
Ibraheem Bakare <bakare.ibraheem.ola@gmail.com>
2023-08-19 17:41:02 +0100

    sql tutorial

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 -- This creates a table called first_table
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_0;
 SHOW DATABASES;
-USE hbtn_0c_0;
+USE hbtn_test_db_0;
 CREATE TABLE first_table (id int, name varchar(256));
