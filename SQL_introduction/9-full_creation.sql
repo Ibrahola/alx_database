40ef2ae8c700ed2a22b59b90207aed7f89ebda38
Ibraheem Bakare <bakare.ibraheem.ola@gmail.com>
2023-08-20 15:18:24 +0100

    sql tutorial

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- This creates a full second_table
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
-USE hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_9;
+USE hbtn_test_db_9;
 CREATE TABLE IF NOT EXISTS second_table (id int, name varchar(256), score int);
 INSERT INTO second_table VALUES ('1', 'John', '10'), ('2', 'Alex', '3'), ('3', 'Bob', '14'), ('4', 'George', '8');
