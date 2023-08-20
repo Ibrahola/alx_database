c940dd20fba61e0b5070780a8a02af00aab1aaff
Ibraheem Bakare <bakare.ibraheem.ola@gmail.com>
2023-08-20 15:33:19 +0100

    sql tutorial

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -4,4 +4,3 @@ USE hbtn_test_db_6;
 CREATE TABLE IF NOT EXISTS  first_table (id int, name varchar(256));
 INSERT INTO first_table VALUES ('1', 'First name'), ('2', 'Last name'), ('3', 'name');
 SELECT *FROM first_table;
-TRUNCATE TABLE first_table;
