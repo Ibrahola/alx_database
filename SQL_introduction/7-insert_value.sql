1a14843cda9d7adf770eb76432edcd90298680ef
Ibraheem Bakare <bakare.ibraheem.ola@gmail.com>
2023-08-20 15:21:49 +0100

    sql tutorial

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,8 +1,5 @@
 -- This is to insert values into table
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
-SHOW DATABASES;
 USE hbtn_0c_0;
 CREATE TABLE IF NOT EXISTS first_table (id int, name varchar(256));
 INSERT INTO first_table VALUES ('89', 'Holberton School');
-SELECT *FROM first_table;
-TRUNCATE TABLE first_table;
