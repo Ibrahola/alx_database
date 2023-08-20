e21840a9890058527b4a1c0c3c2e45a4b4e90dab
Ibraheem Bakare <bakare.ibraheem.ola@gmail.com>
2023-08-20 15:52:19 +0100

    sql tutorial

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
 -- This descibes the first_table.
 CREATE DATABASE IF NOT EXISTS hbtn_test_db_5;
 USE hbtn_test_db_5;
-CREATE TABLE IF NOT EXISTS first_table (id int, name varchar(256));
+CREATE TABLE IF NOT EXISTS first_table (id int NOT NULL AUTO_INCREMENT, name varchar(256));
