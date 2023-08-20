ed9f54143f1292966d116fa203f558e85a294a95
Ibraheem Bakare <bakare.ibraheem.ola@gmail.com>
2023-08-20 16:02:58 +0100

    sql tutorial

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -5,4 +5,3 @@ CREATE TABLE IF NOT EXISTS  first_table (id int, name varchar(256));
 INSERT INTO first_table (id, name) VALUES ('1', 'First name');
 INSERT INTO first_table (id, name) VALUES ('2', 'Last name');
 INSERT INTO first_table (id, name) VALUES ('3', 'name');
-SHOW TABLE;
