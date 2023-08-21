52b743335c289e213227fbd67da60002bbfabb73
Ibraheem Bakare <bakare.ibraheem.ola@gmail.com>
2023-08-21 21:01:23 +0100

    sql tutorial

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -2,4 +2,4 @@
 CREATE DATABASE IF NOT EXISTS hbtn_test_db_4;
 USE hbtn_test_db_4;
 CREATE TABLE IF NOT EXISTS id_not_null (id, int, name varchar(256));
-INSERT INTO id_not_null VALUES ('1', 'Holberton School'), ('1', 'Python is cool'), ('2', 'Holberton'), ('3', 'School'), ('4', 'C is fun');
+INSERT INTO id_not_null VALUES ('1', 'Holberton School'), ('1', 'Python is cool'), ('2', 'Holberton'), ('3', 'School');
