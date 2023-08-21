98040165f36b85c8c462b8db3609d2b868844210
Ibraheem Bakare <bakare.ibraheem.ola@gmail.com>
2023-08-21 20:16:16 +0100

    sql tutorial

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,5 +2,5 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 USE hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-SET PASSWORD FOR 'user_0d_2'@'localhost'="user_0d_2_pwd"
+SET PASSWORD FOR 'user_0d_2'@'localhost'="user_0d_2_pwd1142"
 GRANT ALL PRIVILEGES ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost'
