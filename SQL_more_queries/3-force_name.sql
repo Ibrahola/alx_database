71e6c1fd9b8c2953f258ca3610a6a0dc98862308
Ibraheem Bakare <bakare.ibraheem.ola@gmail.com>
2023-08-21 20:51:55 +0100

    sql tutorial

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,4 +1,4 @@
 -- This create table
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-USE hbtn_0d_2;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_3;
+USE hbtn_test_db_3;
 CREATE TABLE IF NOT EXISTS force_name (id int, name varchar(256));
