31986393322740f9052cab738f0455d6a7ff6c2c
Ibraheem Bakare <bakare.ibraheem.ola@gmail.com>
2023-08-21 20:40:39 +0100

    sql tutorial

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -4,4 +4,4 @@ USE hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 SET PASSWORD FOR 'user_0d_2'@'localhost'="user_0d_2_pwd"
 GRANT USAGE ON *.* TO 'user_0d_2'@'localhost'
-GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost'
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost'
