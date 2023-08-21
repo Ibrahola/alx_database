11d0d40ea69c277f88fe261c4c4fd06af8eca62c
Ibraheem Bakare <bakare.ibraheem.ola@gmail.com>
2023-08-21 20:03:46 +0100

    sql tutorial

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- This creates a user, having password and all privileges in my server.
-CREATE USER IF NOT EXIST 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 SET PASSWORD FOR 'user_0d_1'@'localhost'="user_0d_1_pwd";
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
