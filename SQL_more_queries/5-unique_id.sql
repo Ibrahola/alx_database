3da5282faadada834614b59f98efbaf35cbeb1a7
Ibraheem Bakare <bakare.ibraheem.ola@gmail.com>
2023-08-22 20:59:38 +0100

    sql tutorial

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -2,3 +2,4 @@
 CREATE DATABASE IF NOT EXISTS hbtn_test_db_5;
 USE hbtn_test_db_5;
 CREATE TABLE IF NOT EXISTS unique_id (id int, name varchar(256));
+INSERT INTO unique_id (name) VALUES ('Holberton School');
