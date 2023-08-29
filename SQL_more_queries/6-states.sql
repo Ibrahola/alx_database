8baac40dc63d264f347c946ed9dabdc3db4e7035
Ibraheem Bakare <bakare.ibraheem.ola@gmail.com>
2023-08-29 02:17:24 +0100

    sql tutorial

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -2,5 +2,3 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (id int, name varchar(256));
-INSERT INTO states (id, name) VALUES (10, "New York");
-INSERT INTO states (name) VALUES (1, "Louisiana");
