cb3c5f6d6c429e21eedbee1f540474d471d3dc41
Ibraheem Bakare <bakare.ibraheem.ola@gmail.com>
2023-08-29 03:04:58 +0100

    sql tutorial

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -3,4 +3,4 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (id INT NOT NULL, state_id int, name varchar(256));
-INSERT INTO cities (1, 1, "San Francisco"), (2, 1, "San Diego"), (10, 2, "Page"), (11, 2, "Phoenix")
+INSERT INTO cities ("1", "1", "San Francisco"), ("2", "1", "San Diego"), ("10", "2", "Page"), ("11", "2", "Phoenix")
